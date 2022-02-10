ac21a81079fcfe95eb8cacf6741c2c331f219e43
Landon Swartz <lgsm2n@mail.missouri.edu>
2022-02-10 13:24:49 -0600

    Adding threading to serial port class

--- src/Util/UART_Serial_class.py
+++ src/Util/UART_Serial_class.py
@@ -8,15 +8,26 @@ TODO:
 '''
 
 import serial
+import threading
+from queue import Queue
+
+# may do events for when recieving data causing data to be sent
 
 class UART_Serial(object):
     """For communicating through TX/RX pins"""
+    to_send_queue = Queue(maxsize=0) #sending queue
+    to_read_queue = Queue(maxsize=0) #reading queue
 
     'Constructor'
     def __init__(self):
         self.ser = serial.Serial() #May try to set here everything
         #self.ser.Open_Port(portname)
         print('UART_Serial Created')
+        #listening thread
+        self.listening_thread = threading.Thread(self.Listening_To_Serial(), daemon=True)
+        self.sending_thread = threading.Thread(self.Sending_Serial(), daemon=True)
+        self._lock = threading.Lock() #lock for thread
+
 
     'Opens Serial Port with passed port name'
     def Open_Port(self, portname):
@@ -25,11 +36,20 @@ class UART_Serial(object):
             self.ser.baudrate = 115200  # grbl baudrate
         except serial.SerialException:
             print("Serial port failed to open")
-            return 0; #for failure to open port
+            return 0  # for failure to open port
+
+    #May not need
+    # Get all contents of to read queue
+    def To_Read_Data(self):
+        return self.to_read_queue #send to read queue to requested
+
+    #Add To Send Data
+    def To_Send_Data(self, msg):
+        self.to_send_queue.put(msg)
 
     'Reads Data from Serial Port when called'
     def Read_Data(self):
-        data = self.ser.read(10)  # read ten bytes from port
+        data = self.ser.readline  # read line from port
         print("Read " + data + "from serial port {}".format(self.ser.name))  # may not need this
         return data
 
@@ -63,7 +83,34 @@ class UART_Serial(object):
     def Get_Port_Name(self):
         return self.ser.name
 
+    'Threading Functions for listening and sending'
+    # useful link: https://realpython.com/intro-to-python-threading/
+    # function that will be passed to thread in inition for
+    def Listening_To_Serial(self):
+        to_read_local = self.to_read_queue
+        while(True):
+            #passing local copies
+            #ser_local = self.ser #don't know if needed because not changing data
+            if(self.ser.in_waiting > 0):  # if something in serial port, read it
+                with self._lock: # acquiring lock
+                    to_read_local.put(self.Read_Data())
+                    #self.to_read_queue.put(self.Read_Data())  # putting into queue
+                    self.to_read_queue = to_read_local
+
+    # thread function for sending serial data
+    #TODO check this thread for working with local copy of to send queue
+    def Sending_Serial(self):
+        #to_send_local = self.to_send_queue
+        while(True):
+            if(self.to_send_queue.empty() == False):
+                with self._lock:  # acquiring and releasing lock
+                    self.Write_Data(self.to_send_queue.get()) # send data from queue
+
     'Deconstructor, closes port on exit'
     def __del__(self):
+        # joining daemon threads
+        self.listening_thread.join()
+        self.sending_thread.join()
+        # closing port
         self.Close_Port()
         print('Port closed')
