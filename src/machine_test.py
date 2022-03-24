ca978b91dc2d81f6edfa2f106a68def5263f3320
Landon Swartz <lgsm2n@mail.missouri.edu>
2022-03-24 10:21:22 -0500

    added peripheral script for arduino

---
+++ src/machine_test.py
@@ -0,0 +1,18 @@
+import time
+
+from Machine_Class import Machine
+from time import sleep
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+current_time = time.time()
+
+machine = Machine()
+machine.timelapse_end_date = current_time + (6*60)
+
+sleep(1)
+machine.StartTimelapse()
+
+sleep(10)
