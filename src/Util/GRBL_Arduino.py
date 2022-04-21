91ffa9e37eb32df60ebe52d99874b755477c3acf
LandonSwartz <lgsm2n@umsystem.edu>
2022-04-21 14:06:57 -0500

    GRBL settings auto load into Arduino now

--- src/Util/GRBL_Arduino.py
+++ src/Util/GRBL_Arduino.py
@@ -13,28 +13,21 @@ from time import sleep
 
 class GRBL_Arduino(Arduino):
 
-    #GRBL_Settings = File('Setting_Files/GRBL_settings.txt')
-    #GRBL_Commands = File('Setting_Files/grbl_commands.txt') # may not be able to use
-
+    GRBL_Settings = File('src/Setting_Files/GRBL_settings.txt')
+    
     def __init__(self, portname):
         super(GRBL_Arduino, self).__init__(portname)
         self.ser_port.Write_Data("\r\n\r\n") # wake up grbl
         sleep(2)
         self.ser_port.ser.flushInput()        # flush input messages
+        self.SetGRBLSettings()
         self.HomeCommand()
-
-    def SingleCycleTest(self):
-        single_cycle_commands = self.GRBL_Commands.ReturnFileAsList()
-
-        for command in single_cycle_commands:
-            self.SendCommand(command)
-            sleep(4)
 
     # homing commands wait until we have ok signal, other manual commands go to ok right away
     def SendCommand(self, command):
         try:
             self.Send_Serial(command + '\n')
-            sleep(0.5)
+            sleep(0.3)
             is_ok = self.ser_port.Read_Data()
             while is_ok != 'ok':
                 is_ok = self.ser_port.Read_Data()
@@ -46,13 +39,9 @@ class GRBL_Arduino(Arduino):
 
     # set GRBL Settings to serial port
     def SetGRBLSettings(self):
-        #print('Send GRBL Settings')
-        print('loading settings')
-        #for line in self.GRBL_Settings.ReturnFileAsList():
-            #print(line)
+        logging.info('Loading GRBL settings')
         for line in self.GRBL_Settings.ReturnFileAsList():
             self.Send_Serial(line)
 
     def HomeCommand(self):
         self.SendCommand('$H\n')
-        #self.Send_Serial('$H\n')
