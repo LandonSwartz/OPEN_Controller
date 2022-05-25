59bfb61c7ddeaef60828258f0d3abc9f26e44a8e
Drew Dahlquist <dgdtx5@umsystem.edu>
2022-05-25 20:26:31 +0100

    minor touch ups

--- server/app.py
+++ server/app.py
@@ -2,25 +2,37 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# This section is for checking if the server is reachable
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!<p>'
 
+# This section gets information about the current experiment
 @app.route('/experiment', methods=['GET'])
 def experiment():
+
+    print(request)
+
     return '<p>/experiment hit<p>'
 
+# This section starts a new experiment
 @app.route('/experiment/start', methods=['POST'])
 def experiment_start():
+    
     print(request.json)
+    
     return '<p>/experiment/start hit<p>'
 
+# This section stops the current experiment
 @app.route('/experiment/stop', methods=['PUT'])
 def experiment_stop():
+    
     return '<p>/experiment/stop hit<p>'
 
+# This section (single) cycles the current experiment
 @app.route('/experiment/cycle', methods=['PUT'])
 def experiment_cycle():
+    
     return '<p>/experiment/cycle hit<p>'
 
 if __name__ == '__main__':
